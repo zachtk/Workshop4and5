93301725d9f22ad476e058d9714fc392c504d0ca
Zach Tandara-Kuhns <ztandarakuhn@umass.edu>
2017-03-31 14:01:41 -0400

    fb4

--- app/server.js
+++ app/server.js
@@ -9,3 +9,44 @@ function emulateServerReturn(data, cb) {
     cb(data);
   }, 4);
 }
+
+/**
+ * Given a feed item ID, returns a FeedItem object with references resolved.
+ * Internal to the server, since it's synchronous.
+ */
+function getFeedItemSync(feedItemId) {
+  var feedItem = readDocument('feedItems', feedItemId);
+  // Resolve 'like' counter.
+  feedItem.likeCounter =
+    feedItem.likeCounter.map((id) => readDocument('users', id));
+  // Assuming a StatusUpdate. If we had other types of
+  // FeedItems in the DB, we would
+  // need to check the type and have logic for each type.
+  feedItem.contents.author =
+    readDocument('users', feedItem.contents.author);
+  // Resolve comment author.
+  feedItem.comments.forEach((comment) => {
+    comment.author = readDocument('users', comment.author);
+  });
+  return feedItem;
+}
+
+/**
+ * Emulates a REST call to get the feed data for a particular user.
+ * @param user The ID of the user whose feed we are requesting.
+ * @param cb A Function object, which we will invoke when the Feed's data is available.
+ */
+export function getFeedData(user, cb) {
+  // Get the User object with the id "user".
+  var userData = readDocument('users', user);
+  // Get the Feed object for the user.
+  var feedData = readDocument('feeds', userData.feed);
+  // Map the Feed's FeedItem references to actual FeedItem objects.
+  // Note: While map takes a callback function as an argument, it is
+  // synchronous, not asynchronous. It calls the callback immediately.
+  feedData.contents = feedData.contents.map(getFeedItemSync);
+  // Return FeedData with resolved references.
+  // emulateServerReturn will emulate an asynchronous server operation, which
+  // invokes (calls) the "cb" function some time in the future.
+  emulateServerReturn(feedData, cb);
+}
